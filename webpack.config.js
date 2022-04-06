7b21ed116dc11150083f5b51925a9d1a3a090c82
cola-mars <86696492+cola-mars@users.noreply.github.com>
2022-04-05 22:05:51 -0400

    cola/issue #1: improve website load time & fix node modules

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,44 @@
 const path = require('path');
 const webpack = require('webpack');
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
 // create the main config object
-module.exports ={
-  // create entry property: root of the bunle and the beginning of the dependency graph (the relative path to the client's code)
-  entry: './assets/js/script.js',
+module.exports = {
+  // create entry property: root of the bundle and the beginning of the dependency graph (the relative path to the client's code)
+  entry: {
+    app: './assets/js/script.js',
+    events: './assets/js/events.js',
+    schedule: './assets/js/schedule.js',
+    tickets: './assets/js/tickets.js'
+  },
   // bundle the code from the entry point that we provided and output it to a folder that we specify
   output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'main.bundle.js'
+    filename: "[name].bundle.js",
+    path: __dirname + "/dist"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jpg$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              esModule: false,
+              name(file) {
+                return '[path][name].[ext]';
+              },
+              publicPath: function(url) {
+                return url.replace('../', '/assets/');
+              }
+            }
+          },
+          {
+            loader: 'image-webpack-loader'
+          }
+        ]
+      }
+    ]
   },
   // include webpack
   plugins:[
@@ -16,6 +46,9 @@ module.exports ={
       $: "jquery",
       jQuery: "jquery"
     }),
+    new BundleAnalyzerPlugin({
+      analyzerMode: "static", // the report outputs to an HTML file in the dist folder
+    })
   ],  
   // provide the mode in which we want webpack to run 
   mode: 'development'
